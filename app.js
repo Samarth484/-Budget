a84cf2e8791683db09dbdd76be5b5ead67ee60c2
Samarth Srivastava <samarthsrivastava009@gmail.com>
2020-10-08 00:59:22 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,30 @@
+//BUDGET CONTROLLER
+var budgetController = (function() {
+
+    // some code here
+
+})();
+
+
+
+
+
+//UI CONTROLLER
+
+var UIController = (function() {
+
+    // some code here....
+
+})();
+
+
+
+
+
+// GLOBAL APP CONTROLLER
+
+var controller = (function(budgetCtrl, UICtrl) {
+
+    // some code goes here...
+
+})(budgetController, UIController);
