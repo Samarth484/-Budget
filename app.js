4ebedb28477c77dde3c2e23902c15a3b76f69d86
Samarth Srivastava <samarthsrivastava009@gmail.com>
2020-10-08 01:22:48 +0530

    A little more of the structuring

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //BUDGET CONTROLLER
 var budgetController = (function() {
 
-    // some code here
+    // some code here....
 
 })();
 
@@ -25,6 +25,43 @@ var UIController = (function() {
 
 var controller = (function(budgetCtrl, UICtrl) {
 
-    // some code goes here...
+    document.querySelector(".add__btn").addEventListener("click", function() {
+        console.log("the click works fine!");
+        /*
+        Things that are to be done on either of click or of the press of the ENTER key whose keycode is 13...
+        1.) Get data from the user
+        2.) Add the item to the budget controller
+        3.) Add the item to the UI
+        4.) Update the budget
+        5.) Update the Budget on the UI
+        */
+    });
+
+    // The same thing to be checked on the pressing of the ENTER key...
+    document.addEventListener("keypress", function(event) {
+        if (event.keypress === 13 || event.which === 13)
+            console.log("ENTER key was pressed");
+        /*
+        Things that are to be done on either of click or of the press of the ENTER key whose keycode is 13...
+        1.) Get data from the user
+        2.) Add the item to the budget controller
+        3.) Add the item to the UI
+        4.) Update the budget
+        5.) Update the Budget on the UI
+        */
+    });
 
 })(budgetController, UIController);
+
+
+
+
+
+/*
+Things that are to be done on either of click or of the press of the ENTER key whose keycode is 13...
+1.) Get data from the user
+2.) Add the item to the budget controller
+3.) Add the item to the UI
+4.) Update the budget
+5.) Update the Budget on the UI
+*/
